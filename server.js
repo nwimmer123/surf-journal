a5592359df3435525bc0aca034401bcb032a317e
Noah Wimmer <nwimmer123@yahoo.com>
2016-09-21 12:46:24 -0700

    final commit pre delete

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
 var express = require('express');
 var stormpath = require('express-stormpath');
  
 var app = express();
+
+var compiler = webpack(config);
+ 
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
  
 app.use(stormpath.init(app, {
   web: {
