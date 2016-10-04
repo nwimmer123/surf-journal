7f7104b011edd489425f3473464ec23eb30a50d5
Noah Wimmer <nwimmer123@yahoo.com>
2016-10-03 22:00:41 -0700

    Basic layouts

---
+++ client/src/componets/Base.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link, IndexLink} from 'react-router';
+
+class Base extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="top-bar">
+                    <div className="top-bar-left">
+                        <IndexLink to="/">React App</IndexLink>
+                    </div>
+
+                <div className="top-bar-right">
+                    <Link to="/login">Log in</Link>
+                    <Link to="signup">Sign up</Link>
+                </div>
+
+            </div>
+            {}
+            {this.props.children}
+
+        </div>
+        );
+    }
+}
+
+export default Base;
