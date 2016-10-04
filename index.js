edbe0939149be6069467832eecaee5a4b49cae4b
Noah Wimmer <nwimmer123@yahoo.com>
2016-10-03 20:54:17 -0700

    Imported basic styles

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+let app = express();
+
+app.use(express.static('./server/static'));
+app.use(express.static('./client/dist/'));
+
+app.listen(3000, function() {
+    console.log('TITTIES!!!');
+});
